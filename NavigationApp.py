5048fba2e02a66dfcf8c9163b683d62bd045efb5
NGahman <48140671+NGahman@users.noreply.github.com>
2021-10-25 16:49:57 -0400

    Initialization

---
+++ NavigationApp.py
@@ -0,0 +1,2 @@
+import NavigationGraph
+import NavigationInterface
---
+++ NavigationPath.py
@@ -0,0 +1,11 @@
+class NavigationPath:
+    def __init__(self, instructions, miles, kilometers, time):
+        self.instructions = instructions
+        self.miles = miles
+        self.kilometers = kilometers
+        self.time = time
+
+    def GetInstructions(self):
+        return self.instructions
+    def GetPathLength(self)
+        return (self.miles,self.kilometers, self.time)
